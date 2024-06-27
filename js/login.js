1d88efe922b288e1d50d02ada7a5d1d80796a638
Thiago Ferreira <thiago.oliveira.1446007@sga.pucminas.br>
2024-06-27 10:28:31 -0300

    Update login JavaScript

--- js/login.js
+++ js/login.js
@@ -13,3 +13,39 @@ function toggleShowPassword(id, showIconId, hideIconId) {
         hidePasswordIconElement.style.display = 'none';
     }
 }
+
+/*============ LOGIN ==================*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  const $ = (elemento) => document.querySelector(elemento);
+
+  $("#kc-form-login").addEventListener("submit", function(ev) {
+      ev.preventDefault();
+
+      const string = localStorage.getItem("usuario");
+      if (!string) {
+          alert("Usuário não cadastrado! Cadastre-se antes de fazer login.");
+          return;
+      }
+
+      const usuarioCadastrado = JSON.parse(string);
+      const { senha } = usuarioCadastrado;
+
+      const dadosCorretos = senha === $("#password").value;
+
+      if (!dadosCorretos) {
+          alert("Senha incorreta! Verifique seus dados.");
+          return;
+      }
+
+      setTimeout(function() {
+          alert('Agora podemos ouvir Músicas!!!');
+      });
+
+      alert("Bem-vindo ao Dial");
+
+      if (localStorage.getItem("token") == null) {
+          window.location.href = "music1.html";
+      }
+  });
+});
